a1cfa333f6a454e5b24f0fb9a8687cd87dead8af
duque-maria <mariangelduque@colegioguanenta.edu.co>
2025-01-30 16:29:28 -0500

    Se agregó el programa en python

---
+++ suma_n_numeros.py
@@ -0,0 +1,16 @@
+print("############################")
+print("Suma de los n primeros números naturales")
+print("############################")
+      
+
+# input
+n = input("Digite un número: ")
+n = int(n)
+
+# Processing
+s = (n*(n+1))/2
+
+# Output
+print("#############################")
+print("la suma es : " + str(s))
+print("#############################")
